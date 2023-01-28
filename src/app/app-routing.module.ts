3d16a49239a2a2f91fea0901373deb8a6859d83e
Fernando <femastro@gmail.com>
2023-01-28 19:58:45 -0300

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContainerComponent } from './shared/container/container.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: ContainerComponent,
+    children:[
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+      { path: 'productos', loadChildren: () => import('./pages/productos/productos.module').then(m => m.ProductosModule) },
+      { path: 'info', loadChildren: () => import('./pages/info/info.module').then(m => m.InfoModule) },
+      { path: 'ubicacion', loadChildren: () => import('./pages/ubicacion/ubicacion.module').then(m => m.UbicacionModule) },
+
+    ]
+
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './shared/container/container.component';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
