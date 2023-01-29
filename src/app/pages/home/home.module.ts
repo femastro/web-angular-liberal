f17009bef7ec3ed114736709f77c267e6544ae90
Fernando <femastro@gmail.com>
2023-01-29 03:35:10 -0300

    Se creo parte de la Home

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -12,6 +13,8 @@ import { HomeComponent } from './home.component';
   imports: [
     CommonModule,
     HomeRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
