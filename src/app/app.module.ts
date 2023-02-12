70e0e184d2123044bba2efb67a03dc5c96a05723
Fernando <femastro@gmail.com>
2023-02-12 00:02:55 -0300

    update 11 feb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './shared/container/container.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { ToolbarComponent } from './shared/toolbar/toolbar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
